60b5880f7c1e361efb729dacd7cbd3363d91a897
Junaid Malik <junaid-109@hotmail.com>
2018-11-30 07:46:58 +0000

    added firebase config

--- e-learning/src/app/app.component.ts
+++ e-learning/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {initializeApp, database } from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(){
+        // Initialize Firebase
+        var config = {
+          apiKey: "AIzaSyCJ55vcIM-DR8sbY8NF2zh8aI7E-X0vFK8",
+          authDomain: "e-learning-app-1d02f.firebaseapp.com",
+          databaseURL: "https://e-learning-app-1d02f.firebaseio.com",
+          projectId: "e-learning-app-1d02f",
+          storageBucket: "e-learning-app-1d02f.appspot.com",
+          messagingSenderId: "900334224202"
+        };
+        firebase.initializeApp(config);
+      
+        var root = firebase.database().ref();
+      
+        root.on('value', function(snap) {
+            console.log(snap.val());
+        })  
+  }
+
 }
---
+++ e-learning/src/environments/firebase.config.ts
@@ -0,0 +1,13 @@
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCJ55vcIM-DR8sbY8NF2zh8aI7E-X0vFK8",
+    authDomain: "e-learning-app-1d02f.firebaseapp.com",
+    databaseURL: "https://e-learning-app-1d02f.firebaseio.com",
+    projectId: "e-learning-app-1d02f",
+    storageBucket: "e-learning-app-1d02f.appspot.com",
+    messagingSenderId: "900334224202"
+};
+
+
+
